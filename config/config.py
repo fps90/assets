651a34579552d78a1cfa86eaa8634e461d949252
Team Freedom <167733685+fps90@users.noreply.github.com>
2024-09-29 01:53:03 +0300

    Update config.py

--- config/config.py
+++ config/config.py
@@ -1,62 +1,54 @@
-import re
-from os import getenv
 
+import sys
+from os import getenv
+import os
 from dotenv import load_dotenv
 from pyrogram import filters
 
 load_dotenv()
+import re
 
-API_ID = int(getenv("API_ID","16228886"))
-API_HASH = getenv("API_HASH","b7de6ba01d7af6fc2d99375b5fbd0f82")
-BOT_TOKEN = getenv("BOT_TOKEN","5538104001:AAHH9a9A3CtT9umPFd0TLZbSfCYjgu8H7xU")
+API_ID = int(getenv("API_ID", ""))
+API_HASH = getenv("API_HASH")
+OWNER = int(os.getenv("OWNER", "1854384004"))
+BOT_TOKEN = getenv("BOT_TOKEN")
+Muntazer = getenv("Muntazer", "")
+assistant = getenv("assistant", "")
 MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://omsnkok:muntazer@cluster0.ywxsjao.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", "9999"))
-LOGGER_ID = int(getenv("LOGGER_ID", "-1001854110934"))
-BOTADDLOGS = int(getenv("BOTADDLOGS", "-1001854110934")) # LOGGER_ID Id Also Use No Problem
-GBAN_LOGS = int(getenv("GBAN_LOGS", "-1001854110934"))
-GCAST_USERS = list(map(int, getenv("GCAST_USERS", "2105971379 7137269276 7045191057").split()))
-OWNER_ID = int(getenv("OWNER_ID", 5186954055))
-OWNER = int(getenv("OWNER", 5186954055))
-OWNER_USERNAME = getenv("OWNER_USERNAME","sultan11100")
-HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
+DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", "50000"))  
+LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", ""))
+MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME", "freedom")
+PROTECT_CONTENT = getenv("PROTECT_CONTENT", "True")
+OWNER_ID = list(map(int, getenv("OWNER_ID", "1854384004").split()))
 HEROKU_API_KEY = getenv("HEROKU_API_KEY")
-UPSTREAM_REPO = getenv("UPSTREAM_REPO","https://github.com/AbhiModszYT/AnieXEricaMusic")
-UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
-GIT_TOKEN = getenv("GIT_TOKEN",None)
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/SuperBanSBots")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/AM_YTSUPPORT")
-AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", True))
-AUTO_LEAVE_ASSISTANT_TIME = int(getenv("ASSISTANT_LEAVE_TIME", "5400"))
-SONG_DOWNLOAD_DURATION = int(getenv("SONG_DOWNLOAD_DURATION", 7000))
-SONG_DOWNLOAD_DURATION_LIMIT = int(getenv("SONG_DOWNLOAD_DURATION_LIMIT", 7000))
-DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", "9999")) 
-SPOTIFY_CLIENT_ID = getenv("SPOTIFY_CLIENT_ID", "2a230af10e0a40638dc77c1febb47170")
-SPOTIFY_CLIENT_SECRET = getenv("SPOTIFY_CLIENT_SECRET", "7f92897a59464ddbbf00f06cd6bda7fc")
-PLAYLIST_FETCH_LIMIT = int(getenv("PLAYLIST_FETCH_LIMIT", 25))
-SERVER_PLAYLIST_LIMIT = int(getenv("SERVER_PLAYLIST_LIMIT", "3000"))
-TG_AUDIO_FILESIZE_LIMIT = int(getenv("TG_AUDIO_FILESIZE_LIMIT", 5242880000))
-TG_VIDEO_FILESIZE_LIMIT = int(getenv("TG_VIDEO_FILESIZE_LIMIT", 5242880000))
-STRING1 = getenv("STRING_SESSION", "AgCIVfMAa_Q4giosRUF8lDN7m_Q4m6WeeCM1anQsYqQlsDIkNIljMU7Pgc9mQmF5R1TMKiRb1AF72PwqePJUowquYBCXG1vi4rC325zNOKjI385z5A18aFQx37DNg_E_ArHUzsLfF900n5aJMbVnzAqKUMco4c6sjY6u6NKsUOTWXrOkMMvwgHMNXZSM7SiHR5a9Sxy4RfnYYKVD-Y3leoJ_AA5ppJXTLVs58oTrUnnxqmNhpzFzV3DwuJTuLdHX0yK2H5tHLkLzlOxw2gaW_0G2OZZUHBpi0DjbzmU4nFOSWf8YHJ8xJ5OwWRyBL0_hCJ_BY9gfu8vYL9GFkmgEZb-TCLfPaQAAAAFBhL7oAA")
-STRING2 = getenv("STRING_SESSION2", None)
-STRING3 = getenv("STRING_SESSION3", None)
-STRING4 = getenv("STRING_SESSION4", None)
-STRING5 = getenv("STRING_SESSION5", None)
-STRING6 = getenv("STRING_SESSION6", None)
-BANNED_USERS = filters.user()
-LOG_FILE_NAME = YukkiMusic
-adminlist = {}
-lyrical = {}
-votemode = {}
-autoclean = []
-confirmer = {}
-AMBOT = [
-    "🔎",
-    "🔍",
-    "🧪",
-    "ᴘʟꜱ ᴡᴀɪᴛ..",
-    "ᴘʀᴏᴄᴇꜱꜱɪɴɢ..",
-]
-
+HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
+UPSTREAM_REPO = getenv("UPSTREAM_REPO","https://github.com/fps90/Hot")
+UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
+GIT_TOKEN = getenv("GIT_TOKEN","")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/xl444")
+SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/vvyvv6") 
+AUTO_LEAVING_ASSISTANT = getenv("AUTO_LEAVING_ASSISTANT", False)
+AUTO_LEAVE_ASSISTANT_TIME = int(getenv("ASSISTANT_LEAVE_TIME", "50000"))
+AUTO_SUGGESTION_TIME = int(getenv("AUTO_SUGGESTION_TIME", "3000"))
+AUTO_SUGGESTION_MODE = getenv("AUTO_SUGGESTION_MODE", False)
+PRIVATE_BOT_MODE = getenv("PRIVATE_BOT_MODE", "False")
+YOUTUBE_DOWNLOAD_EDIT_SLEEP = int(getenv("YOUTUBE_EDIT_SLEEP", "3"))
+TELEGRAM_DOWNLOAD_EDIT_SLEEP = int(getenv("TELEGRAM_EDIT_SLEEP", "5"))
+GITHUB_REPO = getenv("GITHUB_REPO","")
+SPOTIFY_CLIENT_ID = getenv("SPOTIFY_CLIENT_ID", "19609edb1b9f4ed7be0c8c1342039362")
+SPOTIFY_CLIENT_SECRET = getenv("SPOTIFY_CLIENT_SECRET", "409e31d3ddd64af08cfcc3b0f064fcbe")
+VIDEO_STREAM_LIMIT = int(getenv("VIDEO_STREAM_LIMIT", "5"))
+SERVER_PLAYLIST_LIMIT = int(getenv("SERVER_PLAYLIST_LIMIT", "50"))
+PLAYLIST_FETCH_LIMIT = int(getenv("PLAYLIST_FETCH_LIMIT", "50"))
+CLEANMODE_DELETE_MINS = int(getenv("CLEANMODE_MINS", "5")) 
+TG_AUDIO_FILESIZE_LIMIT = int(getenv("TG_AUDIO_FILESIZE_LIMIT", "2147483648")) 
+TG_VIDEO_FILESIZE_LIMIT = int(getenv("TG_VIDEO_FILESIZE_LIMIT", "2147483648")) 
+SET_CMDS = getenv("SET_CMDS", "False")
+STRING1 = getenv("STRING_SESSION", "")
+STRING2 = getenv("STRING_SESSION2", "")
+STRING3 = getenv("STRING_SESSION3", "")
+STRING4 = getenv("STRING_SESSION4", "")
+STRING5 = getenv("STRING_SESSION5", "")
 START_IMG_URL = getenv("START_IMG_URL", "https://i.ibb.co/82xJS5p/image.jpg")
 PING_IMG_URL = getenv("PING_IMG_URL", "https://i.ibb.co/HnC5B2R/image.jpg")
 PLAYLIST_IMG_URL = "https://i.ibb.co/Cn9P7KK/image.jpg"
@@ -78,15 +70,98 @@ def time_to_seconds(time):
 
 DURATION_LIMIT = int(time_to_seconds(f"{DURATION_LIMIT_MIN}:00"))
 
+if MUSIC_BOT_NAME is None:
+    MUSIC_BOT_NAME = "Music player"
 
 if SUPPORT_CHANNEL:
     if not re.match("(?:http|https)://", SUPPORT_CHANNEL):
-        raise SystemExit(
+        print(
             "[ERROR] - Your SUPPORT_CHANNEL url is wrong. Please ensure that it starts with https://"
         )
+        sys.exit()
 
-if SUPPORT_CHAT:
-    if not re.match("(?:http|https)://", SUPPORT_CHAT):
-        raise SystemExit(
-            "[ERROR] - Your SUPPORT_CHAT url is wrong. Please ensure that it starts with https://"
+if SUPPORT_GROUP:
+    if not re.match("(?:http|https)://", SUPPORT_GROUP):
+        print(
+            "[ERROR] - Your SUPPORT_GROUP url is wrong. Please ensure that it starts with https://"
         )
+        sys.exit()
+
+if UPSTREAM_REPO:
+    if not re.match("(?:http|https)://", UPSTREAM_REPO):
+        print(
+            "[ERROR] - Your UPSTREAM_REPO url is wrong. Please ensure that it starts with https://"
+        )
+        sys.exit()
+
+
+
+if STATS_IMG_URL:
+    if STATS_IMG_URL != "https://telegra.ph/file/4dd9e2c231eaf7c290404.jpg":
+        if not re.match("(?:http|https)://", STATS_IMG_URL):
+            print(
+                "[ERROR] - Your STATS_IMG_URL url is wrong. Please ensure that it starts with https://"
+            )
+            sys.exit()
+
+
+if TELEGRAM_AUDIO_URL:
+    if TELEGRAM_AUDIO_URL != "https://telegra.ph/file/8234d704952738ebcda7f.jpg":
+        if not re.match("(?:http|https)://", TELEGRAM_AUDIO_URL):
+            print(
+                "[ERROR] - Your TELEGRAM_AUDIO_URL url is wrong. Please ensure that it starts with https://"
+            )
+            sys.exit()
+
+
+if STREAM_IMG_URL:
+    if STREAM_IMG_URL != "https://telegra.ph/file/e24f4a5f695ec5576a8f3.jpg":
+        if not re.match("(?:http|https)://", STREAM_IMG_URL):
+            print(
+                "[ERROR] - Your STREAM_IMG_URL url is wrong. Please ensure that it starts with https://"
+            )
+            sys.exit()
+
+
+if SOUNCLOUD_IMG_URL:
+    if SOUNCLOUD_IMG_URL != "https://telegra.ph/file/7645d1e04021323c21db9.jpg":
+        if not re.match("(?:http|https)://", SOUNCLOUD_IMG_URL):
+            print(
+                "[ERROR] - Your SOUNCLOUD_IMG_URL url is wrong. Please ensure that it starts with https://"
+            )
+            sys.exit()
+
+if YOUTUBE_IMG_URL:
+    if YOUTUBE_IMG_URL != "https://telegra.ph/file/76d29aa31c40a7f026d7e.jpg":
+        if not re.match("(?:http|https)://", YOUTUBE_IMG_URL):
+            print(
+                "[ERROR] - Your YOUTUBE_IMG_URL url is wrong. Please ensure that it starts with https://"
+            )
+            sys.exit()
+
+
+if TELEGRAM_VIDEO_URL:
+    if TELEGRAM_VIDEO_URL != "https://telegra.ph/file/8d02ff3bde400e465219a.jpg":
+        if not re.match("(?:http|https)://", TELEGRAM_VIDEO_URL):
+            print(
+                "[ERROR] - Your TELEGRAM_VIDEO_URL url is wrong. Please ensure that it starts with https://"
+            )
+            sys.exit()
+
+
+BANNED_USERS = filters.user()
+YTDOWNLOADER = 1
+LOG = 2
+LOG_FILE_NAME = "Yukkilogs.txt"
+adminlist = {}
+lyrical = {}
+chatstats = {}
+userstats = {}
+clean = {}
+autoclean = []
+PHOTO = list(
+    filter(
+        None,
+        getenv("PHOTO_LINKS", "").split(),
+    )
+)
